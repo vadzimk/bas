92c4b27444d5f48defb057ab72010730a24dbcda
Vadzim <vadzimk@hotmail.com>
2023-01-20 03:49:00 -0800

    fix cross-origin request blocked

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -5,14 +5,14 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://127.0.0.1:5000',
+      target: 'http://localhost:5000',
       changeOrigin: true,
     })
   );
     app.use(
     '/results',
     createProxyMiddleware({
-      target: 'http://127.0.0.1:5000',
+      target: 'http://localhost:5000',
       changeOrigin: true,
     })
   );
